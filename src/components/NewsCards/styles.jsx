240c8cbb6a63ef1dfba8bcb58f428de264ef2353
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-04-09 19:05:31 +0100

    Add files via upload

---
+++ src/components/NewsCards/styles.jsx
@@ -0,0 +1,41 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles({
+    container:{
+        padding:'0 5%',
+        width:'100%',
+        margin: 0,
+    },
+    ActiveContainer:{
+        padding:'0 10px',
+        width:'100%',
+        margin: 0,
+        height:'100%',
+    },
+    card:{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'space-between',
+        alignItems:'center',
+        height:'60vh',
+        width:'100%',
+        paddingTop:'20%',
+        borderRadius:10,
+        color:'white',
+        paddingBottom:'10%',
+        marginTop:'45%'
+    },
+    infoCard:{
+        display:'flex',
+        flexDirection:'column',
+        textAlign:'center'
+    },
+    title:{
+        background:'#7d3cff',
+        padding:'10px',
+        borderRadius:'10px'
+    }
+
+});
+
+
